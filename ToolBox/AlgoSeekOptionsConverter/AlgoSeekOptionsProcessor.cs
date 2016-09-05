2576bed89859b34f327d57f4f3d5820dd66f8d07
jaredbroad <jaredbroad@gmail.com>
2016-09-05 16:00:23 -0400

    Stored strings in processing queue instead of base data for memory

--- ToolBox/AlgoSeekOptionsConverter/AlgoSeekOptionsProcessor.cs
+++ ToolBox/AlgoSeekOptionsConverter/AlgoSeekOptionsProcessor.cs
@@ -16,10 +16,11 @@
 using System;
 using QuantConnect.Data;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using QuantConnect.Data.Consolidators;
 using QuantConnect.Data.Market;
-using QuantConnect.Logging;
+using QuantConnect.Util;
+using System.Linq;
 
 namespace QuantConnect.ToolBox.AlgoSeekOptionsConverter
 {
@@ -32,25 +33,58 @@ namespace QuantConnect.ToolBox.AlgoSeekOptionsConverter
         private Symbol _symbol;
         private TickType _tickType;
         private Resolution _resolution;
-        private Queue<BaseData> _queue; 
-        private string _destinationDirectory;
+        private Queue<string> _queue; 
+        private string _dataDirectory;
         private IDataConsolidator _consolidator;
+        private DateTime _referenceDate;
+        private static string[] _windowsRestrictedNames =
+        {
+            "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5",
+            "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5",
+            "LPT6", "LPT7", "LPT8", "LPT9"
+        };
+
+        /// <summary>
+        /// Zip entry name for the option contract
+        /// </summary>
+        public string EntryPath
+        {
+            get { return LeanData.GenerateZipEntryName(_symbol, _referenceDate, _resolution, _tickType); }
+        }
+
+        /// <summary>
+        /// Zip file path for the option contract collection
+        /// </summary>
+        public string ZipPath
+        {
+            get { return Path.Combine(_dataDirectory, LeanData.GenerateRelativeZipFilePath(Safe(_symbol), _referenceDate, _resolution, _tickType).Replace(".zip", string.Empty)) + ".zip"; }
+        }
+
+        /// <summary>
+        /// Output base data queue for processing in memory
+        /// </summary>
+        public Queue<string> Queue
+        {
+            get { return _queue; }
+        }
 
         /// <summary>
         /// Create a new AlgoSeekOptionsProcessor for enquing consolidated bars and flushing them to disk
         /// </summary>
         /// <param name="symbol">Symbol for the processor</param>
+        /// <param name="date">Reference date for the processor</param>
         /// <param name="tickType">TradeBar or QuoteBar to generate</param>
         /// <param name="resolution">Resolution to consolidate</param>
-        /// <param name="destinationDirectory">Data directory for LEAN</param>
-        public AlgoSeekOptionsProcessor(Symbol symbol, TickType tickType, Resolution resolution, string destinationDirectory)
+        /// <param name="dataDirectory">Data directory for LEAN</param>
+        public AlgoSeekOptionsProcessor(Symbol symbol, DateTime date, TickType tickType, Resolution resolution, string dataDirectory)
         {
-            _symbol = symbol;
+            _symbol = Safe(symbol);
             _tickType = tickType;
             _resolution = resolution;
-            _queue = new Queue<BaseData>();
-            _destinationDirectory = destinationDirectory;
-
+            _queue = new Queue<string>();
+            _dataDirectory = dataDirectory;
+            _referenceDate = date;
+            
             // Setup the consolidator for the requested resolution
             _consolidator = new PassthroughConsolidator();
             if (resolution != Resolution.Tick)
@@ -68,7 +102,7 @@ namespace QuantConnect.ToolBox.AlgoSeekOptionsConverter
             // On consolidating the bars put the bar into a queue in memory to be written to disk later.
             _consolidator.DataConsolidated += (sender, consolidated) =>
             {
-                _queue.Enqueue(consolidated);
+                _queue.Enqueue(ToCsv(consolidated));
             };
         }
 
@@ -90,8 +124,9 @@ namespace QuantConnect.ToolBox.AlgoSeekOptionsConverter
         /// Write the in memory queues to the disk.
         /// </summary>
         /// <param name="frontierTime">Current foremost tick time</param>
+        /// <param name="inMemoryProcessing">Process this option symbol entirely in memory</param>
         /// <param name="finalFlush">Indicates is this is the final push to disk at the end of the data</param>
-        public void FlushBuffer(DateTime frontierTime, bool finalFlush = false)
+        public void FlushBuffer(DateTime frontierTime, bool inMemoryProcessing, bool finalFlush)
         {
             //Force the consolidation if time has past the bar
             _consolidator.Scan(frontierTime);
@@ -99,18 +134,50 @@ namespace QuantConnect.ToolBox.AlgoSeekOptionsConverter
             // If this is the final packet dump it to the queue
             if (finalFlush && _consolidator.WorkingData != null)
             {
-                _queue.Enqueue(_consolidator.WorkingData);
+                _queue.Enqueue(ToCsv(_consolidator.WorkingData));  
             }
+
+            // No need to write to disk
+            if (inMemoryProcessing) return;
 
             // Purge the queue to disk if there's work to do:
             if (_queue.Count == 0) return;
-            using (var writer = new LeanOptionsWriter(_destinationDirectory, _symbol, frontierTime, _resolution, _tickType))
+
+            using (var writer = new LeanOptionsWriter(_dataDirectory, _symbol, frontierTime, _resolution, _tickType))
             {
                 while (_queue.Count > 0)
                 {
                     writer.WriteEntry(_queue.Dequeue());
                 }
             }
+        }
+
+        /// <summary>
+        /// Add filtering to safe check the symbol for windows environments
+        /// </summary>
+        /// <param name="symbol">Symbol to rename if required</param>
+        /// <returns>Renamed symbol for reserved names</returns>
+        private static Symbol Safe(Symbol symbol)
+        {
+            if (OS.IsWindows)
+            {
+                if (_windowsRestrictedNames.Contains(symbol.Value))
+                {
+                    symbol = Symbol.CreateOption("_" + symbol.Value, symbol.ID.Market, symbol.ID.OptionStyle,
+                        symbol.ID.OptionRight, symbol.ID.StrikePrice, symbol.ID.Date);
+                }
+            }
+            return symbol;
+        }
+
+        /// <summary>
+        /// Convert the basedata to a string.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string ToCsv(BaseData data)
+        {
+            return LeanData.GenerateLine(data, data.Symbol.ID.SecurityType, _resolution);
         }
     }
 
